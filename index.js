c88f18564cfe49dadf26f5057ce9ad62a321c72d
Gabriel <gabrielgaara73@gmail.com>
2024-06-25 19:11:40 -0300

    Commit do exercicio do modulo DOM

---
+++ index.js
@@ -0,0 +1,19 @@
+// seletores:
+const numberA = document.getElementById("number-a")
+const numberB = document.getElementById("number-b")
+const submit = document.getElementById("inputForm")
+
+// Validação do formulario:
+const validation = () =>{
+    if(numberB.valuer > numberA.value){
+        alert("Formulário valido")
+    }else{
+        alert("Formulário inválido")
+    }
+}
+
+// Evento:
+submit.addEventListener("click", (e)=>{
+    e.preventDefault()
+    validation()
+})
